fc036a48b31f0879963b6d0182c26866721e3bac
Lee JaeGeun <ljg95924@github.com>
2022-03-10 20:06:57 +0900

    124나라의숫자: 정확성테스트 통과

--- src/연습문제/oneTwoFour나라의숫자/Solution.java
+++ src/연습문제/oneTwoFour나라의숫자/Solution.java
@@ -1,7 +1,7 @@
 package 연습문제.oneTwoFour나라의숫자;
 
 public class Solution {
-    int count;
+    long count;
     StringBuilder builder = new StringBuilder();
 
     public String solution(int n) {
@@ -17,9 +17,8 @@ public class Solution {
         // 3개 ->1자리 / 9개 ->2자리 / 27 ->3자리 / 81 / 243 / 729 / 2187 /
         //int count = 0;
         StringBuilder builder = new StringBuilder();
-        //this.count = count;
         this.builder = builder;
-        int powSum = 0;
+        long powSum = 0;
         int index = 0;
         for (int i = 0; i < n; i++) {
             if (powSum + Math.pow(3, i) <= n) {
@@ -31,12 +30,8 @@ public class Solution {
 
 
         this.count = n - powSum;
-/*        System.out.println("n" + n);
-        System.out.println("powSum" + powSum);
-        System.out.println("count" + count);
-        System.out.println("index" + index);*/
         processing(index);
-        System.out.println("최종:" + builder.toString());
+        //System.out.println(n + " : " + builder.toString());
         return builder.toString();
     }
 
@@ -54,8 +49,7 @@ public class Solution {
             } else if (builder.charAt(index - 1) == '4') {
                 builder.setCharAt(index - 1, '1');
 
-                for (int i = index - 1; i <= 0; i--) {
-                    System.out.println(index - 1);
+                for (int i = index - 2; i >= 0; i--) {
                     if (builder.charAt(i) == '1') {
                         builder.setCharAt(i, '2');
                         break;
@@ -63,7 +57,19 @@ public class Solution {
                         builder.setCharAt(i, '4');
                         break;
                     } else if (builder.charAt(i) == '4') {
-                        builder.setCharAt(i, '1');
+                        for (int j = i; j >= 0; j--) {
+                            if (builder.charAt(j) == '4') {
+                                builder.setCharAt(j, '1');
+                            } else if (builder.charAt(j) == '1') {
+                                builder.setCharAt(j, '2');
+                                break;
+                            } else {
+                                builder.setCharAt(j, '4');
+                                break;
+                            }
+                        }
+                        break;
+                        //i = index - 1;
                     }
                 }
 
